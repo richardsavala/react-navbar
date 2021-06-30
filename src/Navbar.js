acb3553cbe8f1d71ca52a76f7c26e6f52288bfd0
Richard <82630326+richardsavala@users.noreply.github.com>
2021-06-30 00:00:06 -0600

    Add files via upload
    navbar - data
    in NavBar.js added map

--- src/Navbar.js
+++ src/Navbar.js
@@ -16,18 +16,10 @@ const Navbar = () => {
           </div>
           <div className="links-container show-container">
             <ul className="links">
-              <li>
-                <a href="#">home</a>
-              </li>
-              <li>
-                <a href="#">about</a>
-              </li>
-              <li>
-                <a href="#">contact</a>
-              </li>
-              <li>
-                <a href="#">products</a>
-              </li>
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return;
+              })}
             </ul>
           </div>
           <ul className="social-icons">
