67580c501d6c2f85e9c2f8f882ae7d3b73337ca2
Richard <82630326+richardsavala@users.noreply.github.com>
2021-07-01 08:46:59 -0600

    Add files via upload
    navbar - useRef
    in NavBar.js added useEffect to display the appropriate px height for ul 'links'

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,18 @@ import logo from "./logo.svg";
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
 
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    console.log(linksHeight);
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
   return (
     <nav>
       <h4>
@@ -20,8 +31,8 @@ const Navbar = () => {
             </button>
           </div>
 
-          <div className="links-container ">
-            <ul className="links">
+          <div className="links-container" ref={linksContainerRef}>
+            <ul className="links" ref={linksRef}>
               {links.map((link) => {
                 const { id, url, text } = link;
                 return (
